e75b1c5980c25809a45ad37d6950872f5b27f72d
JDW <jd-wheaton@outlook.com>
2017-11-23 15:16:18 -0500

    Added firebase hook

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB8QeGlu9FoKi_W8pS9mT1EG12GYZNvYo0",
+  authDomain: "catch-of-the-day-c0904.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-c0904.firebaseio.com"
+});
+
+export default base;
